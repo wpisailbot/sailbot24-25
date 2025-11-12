e0298486b595d38e350a17577bcc64cbd90ae234
sailbot <wpisailbotmqp@gmail.com>
2025-11-12 17:24:29 -0500

    fixed service

--- startup/run.sh
+++ startup/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Sourcing environment"
 source /opt/ros/humble/setup.bash
+source /usr/bin/python3
 source /home/sailbot/ros2_ws/install/local_setup.bash
 source /home/sailbot/sailbot24-25/sailbot_ws/install/local_setup.bash
 echo "Launching server"
-python3 /opt/sailbot/startup_server.py
+python3 /home/sailbot/sailbot24-25/startup/startup_server.py #/opt/sailbot/startup_server.py
