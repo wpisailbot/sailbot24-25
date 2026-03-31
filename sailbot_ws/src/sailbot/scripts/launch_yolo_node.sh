294eecf9fef128f93f99532484ce28a98c9baede
sailbot <wpisailbotmqp@gmail.com>
2026-03-31 17:49:31 -0400

    Stashing uncommited yolo launch changes! do better version control !

--- sailbot_ws/src/sailbot/scripts/launch_yolo_node.sh
+++ sailbot_ws/src/sailbot/scripts/launch_yolo_node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/bolongli/Sailbot/sailbot24-25
+cd /home/sailbot/sailbot24-25
 source .venv/bin/activate
 source /opt/ros/humble/setup.bash
-source /home/bolongli/Sailbot/sailbot24-25/sailbot_ws/install/setup.bash
-exec python3 /home/bolongli/Sailbot/sailbot24-25/sailbot_ws/src/sailbot/sailbot/buoy_detection_yolo.py "$@"
+source /home/sailbot/sailbot24-25/sailbot_ws/install/setup.bash
+exec python3 /home/sailbot/sailbot24-25/sailbot_ws/src/sailbot/sailbot/buoy_detection_yolo.py "$@"
