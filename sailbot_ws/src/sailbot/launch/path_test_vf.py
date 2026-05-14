11e3d77fa4b5cb6f2bfcf9564df304e4ef85489d
sailbot <wpisailbotmqp@gmail.com>
2026-05-14 15:39:22 -0400

    fix damper env

--- sailbot_ws/src/sailbot/launch/path_test_vf.py
+++ sailbot_ws/src/sailbot/launch/path_test_vf.py
@@ -44,7 +44,7 @@ def generate_launch_description():
     )
     cv_node = Node(
         package='sailbot',
-        executable='buoy_detection',
+        executable='launch_yolo_node.sh',
         name='buoy_detection',
         namespace='',
         output='screen',
